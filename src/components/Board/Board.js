dd5b23e0fca56cd5fd5f1baa8f2f4f6bff8861c9
shivansh0017 <shivansh7623@gmail.com>
2024-07-26 01:54:42 +0530

    added seach bar

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -5,8 +5,8 @@ import GameSquare from '../../components/Board/GameSquare/GameSquare';
 import ColumnCategorySquare from './Categories/ColumnCategorySquare/ColumnCategorySquare';
 import RowCategorySquare from './Categories/RowCategorySquare/RowCategorySquare';
 import LogoSquare from './LogoSquare/LogoSquare';
-import { MODAL_STATES, SEARCH_PLAYER, STATUS } from '../../utilities/constants';
 import SearchPlayer from '../SearchPlayer/SearchPlayer';
+import { MODAL_STATES, SEARCH_PLAYER, STATUS } from '../../utilities/constants';
 import './Board.css';
 
 function Board() {
@@ -25,37 +25,38 @@ function Board() {
     dispatch(addMarkToBoard(index));
   }
 
-  const handleSearchPlayer = () => {
-    dispatch(toggleSearchPlayer());
+  const handleSearchPlayer = (index) => {
+    dispatch(toggleSearchPlayer(index));
   }
 
   return (
     <div className='board-container'>
       <div className='logo-square'>
-        <LogoSquare/>
+        <LogoSquare />
       </div>
       <div className='column-categories'>
-        <ColumnCategorySquare index={0}/>
-        <ColumnCategorySquare index={1}/>
-        <ColumnCategorySquare index={2}/>
+        <ColumnCategorySquare index={0} />
+        <ColumnCategorySquare index={1} />
+        <ColumnCategorySquare index={2} />
       </div>
       <div className='row-categories'>
-        <RowCategorySquare index={0}/>
-        <RowCategorySquare index={1}/>
-        <RowCategorySquare index={2}/>
+        <RowCategorySquare index={0} />
+        <RowCategorySquare index={1} />
+        <RowCategorySquare index={2} />
       </div>
-      <div className='board' onClick={(event) => handleBoardClick(event)}>
-        <GameSquare cell={board[0]} index={0} key={0} />
-        <GameSquare cell={board[1]} index={1} key={1} />
-        <GameSquare cell={board[2]} index={2} key={2} />
-        <GameSquare cell={board[3]} index={3} key={3} />
-        <GameSquare cell={board[4]} index={4} key={4} />
-        <GameSquare cell={board[5]} index={5} key={5} />
-        <GameSquare cell={board[6]} index={6} key={6} />
-        <GameSquare cell={board[7]} index={7} key={7} />
-        <GameSquare cell={board[8]} index={8} key={8} />
+      {/* <div className='board' onClick={(event) => handleBoardClick(event)}> */}
+      <div className='board' onClick={() => handleSearchPlayer()}>
+        <GameSquare cell={board[0]} index={0}/>
+        <GameSquare cell={board[1]} index={1}/>
+        <GameSquare cell={board[2]} index={2}/>
+        <GameSquare cell={board[3]} index={3}/>
+        <GameSquare cell={board[4]} index={4}/>
+        <GameSquare cell={board[5]} index={5}/>
+        <GameSquare cell={board[6]} index={6}/>
+        <GameSquare cell={board[7]} index={7}/>
+        <GameSquare cell={board[8]} index={8}/>
       </div>
-      {/* <div className='hello'>Hello World</div> */}
+      {searchPlayer === SEARCH_PLAYER.YES && <SearchPlayer />}
     </div>
   )
 }
--- src/components/SearchPlayer/SearchPlayer.js
+++ src/components/SearchPlayer/SearchPlayer.js
@@ -1,8 +1,26 @@
+import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { toggleSearchPlayer } from "../../slices/gameSlice";
+
 const SearchPlayer = () => {
-    return(
-        <>
-        <div>Hello Everyone Good morning</div>
-        </>
+    const dispatch = useDispatch()
+    const handleCancel = () => {
+        dispatch(toggleSearchPlayer())
+    }
+    
+    return (
+        <div className='search-player'>
+            <div className='search-player-container'>
+                <div className='search-player-wrapper'>
+                    <h3 className='search-player-heading'>Player Search</h3>
+                    <p className='search-player-description'>Find a past or present player from row and from column.</p>
+                    <input className='search-player-input' placeholder='Search Player...'></input>
+                </div>
+                <div className='search-player-cancel-container'>
+                    <button className='search-player-cancel' onClick={handleCancel}>Cancel</button>
+                </div>
+            </div>
+        </div>
     )
 }
 
