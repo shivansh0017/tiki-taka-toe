c8441fb37343ebbb823806355e40cbbedda1b2a8
shivansh0017 <shivansh7623@gmail.com>
2024-07-28 13:48:03 +0530

    added Bukayo Saka to players.json

--- src/components/Board/SearchResult/SearchResult.js
+++ src/components/Board/SearchResult/SearchResult.js
@@ -6,8 +6,8 @@ const SearchReults = ({result, rowId, colId}) => {
             // alert(
             //     `You clicked on ${result.n}. The rowId is ${rowId} and the colId is ${colId}`
             // )
-            // alert(`The object is ${result}. The object name is ${result.n}. The object teams array is ${result.t}. The rowId is ${rowId} and the colId is ${colId}`)
-            // checkForPlayerValidity(result, rowId, colId)
+            alert(`The object is ${result}. The object name is ${result.n}. The object teams array is ${result.t}. The rowId is ${rowId} and the colId is ${colId}`)
+            checkForPlayerValidity(result, rowId, colId)
             }}>
             {result.n}
         </div>
